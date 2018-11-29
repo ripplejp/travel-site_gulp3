6b84df81f1fab261c432909ca933b42d2ab57b2d
ripplejp <45304934+ripplejp@users.noreply.github.com>
2018-11-29 17:26:31 +0900

    Conpleted gulp build task.

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ require('./gulp/tasks/styles.js');
 require('./gulp/tasks/watch.js');
 require('./gulp/tasks/sprite.js');
 require('./gulp/tasks/scripts.js');
+require('./gulp/tasks/modernizr.js');
+require('./gulp/tasks/build.js');
 
 // gulp.task('default', function() {
 // 	console.log("Hooray - you created a Gulp task.");
